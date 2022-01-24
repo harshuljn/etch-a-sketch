8173e4037e66498b71f109960348a85d6ecac37a
Harshul Jain <harshuljain2047@gmail.com>
2022-01-24 20:40:55 +0530

    Add reset button

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 const sketchpad = document.querySelector("#sketchpad");
-let gridSide = 32;
-for (let i = 0; i < gridSide ** 2; i++) {
-	const gridPixel = document.createElement("div");
-	gridPixel.style.flex = `0 0 ${100 / gridSide}%`;
-	gridPixel.classList.add("gridPixel");
-	sketchpad.appendChild(gridPixel);
+
+function setGrid() {
+	const pixels = document.querySelectorAll(".gridPixel");
+	pixels.forEach((pixel) =>
+		pixel.addEventListener("mouseover", () => {
+			pixel.classList.add("colored");
+		})
+	);
 }
 
-const pixels = document.querySelectorAll(".gridPixel");
-pixels.forEach((pixel) =>
-	pixel.addEventListener("mouseover", () => {
-		pixel.classList.add("colored");
-	})
-);
+function createGrid(gridSide = 32) {
+	for (let i = 0; i < gridSide ** 2; i++) {
+		const gridPixel = document.createElement("div");
+		gridPixel.style.flex = `0 0 ${100 / gridSide}%`;
+		gridPixel.classList.add("gridPixel");
+		sketchpad.appendChild(gridPixel);
+	}
+	setGrid();
+}
+
+function reset() {
+	while (sketchpad.firstChild) {
+		sketchpad.removeChild(sketchpad.lastChild);
+	}
+	let gridSide = parseInt(prompt("Enter grid length"));
+	gridSide = Math.min(72, gridSide);
+	createGrid(gridSide);
+}
+
+const resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener("click", reset);
+createGrid();
